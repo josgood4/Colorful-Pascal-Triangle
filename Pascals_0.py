0e54a98e5e5e9114c2f8b8d8203a11a1e8726b50
josgood4 <jwosgood4@gmail.com>
2017-04-04 20:34:07 -0400

    genModPascal()
    Changed generatePascal() to genModPascal() (and fixed functionality
    issues)

--- Pascals_0.py
+++ Pascals_0.py
@@ -10,21 +10,20 @@ def nCr(n,r):
 # generatesPascal's triangle in the form of a list of lists,
 #   where the inner list contains the numbers in each column and
 #   the outer list contains each row
-def generatePascal(rowMax):
-  outerList = [[1]]
-  for n in range(2,rowMax+1):
+def genModPascal(mod, rowMax):
+  outerList = []
+  for n in range(0,rowMax+1):
     innerList = []
     for k in range(0,n+1):
-      print(n, ", ", k)
-      innerList.append(nCr(n,k))
+      ##print(n, ", ", k)
+      innerList.append(nCr(n,k) % mod)
     outerList.append(innerList)
   return outerList
 
-
 # print the result of generatePascal() - for testing purposes
-def testGeneratePascal(rowMax):
-  triangle = generatePascal(rowMax)
+def testGeneratePascal(mod, rowMax):
+  triangle = genModPascal(mod, rowMax)
   for eachRow in triangle:
     print(eachRow)
 
-testGeneratePascal(5)
+testGeneratePascal(2, 16)
