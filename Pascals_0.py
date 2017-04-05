2d27bd43001233240d059bc3c908374cac2e10ed
josgood4 <jwosgood4@gmail.com>
2017-04-04 20:28:04 -0400

    testGeneratePascal()
    Fixed and improved generatePascal() and nCr().
    Also created testGeneratePascal()

--- Pascals_0.py
+++ Pascals_0.py
@@ -1,11 +1,30 @@
 from turtle import *
 import math
 
+# calculates nCr
 def nCr(n,r):
-  return math.factorial(n) // (math.factorial(r)*math.factorial(r-n))
+  if n >= r and r >= 0:
+    return math.factorial(n) // (math.factorial(r)*math.factorial(n-r))
 
+
+# generatesPascal's triangle in the form of a list of lists,
+#   where the inner list contains the numbers in each column and
+#   the outer list contains each row
 def generatePascal(rowMax):
-  for n in range(rowMax):
-    i = n
-    for k in range(i):
-      print(str(nCr(n,k)))
+  outerList = [[1]]
+  for n in range(2,rowMax+1):
+    innerList = []
+    for k in range(0,n+1):
+      print(n, ", ", k)
+      innerList.append(nCr(n,k))
+    outerList.append(innerList)
+  return outerList
+
+
+# print the result of generatePascal() - for testing purposes
+def testGeneratePascal(rowMax):
+  triangle = generatePascal(rowMax)
+  for eachRow in triangle:
+    print(eachRow)
+
+testGeneratePascal(5)
