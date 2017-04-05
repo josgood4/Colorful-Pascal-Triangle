d3cc2e3c2c54b72e80bdc3aecddf7982b5e375d5
josgood4 <jwosgood4@gmail.com>
2017-04-04 20:00:42 -0400

    Update Pascals_0.py

--- Pascals_0.py
+++ Pascals_0.py
@@ -1,1 +1,11 @@
+from turtle import *
+import math
 
+def nCr(n,r):
+  return math.factorial(n) // (math.factorial(r)*math.factorial(r-n))
+
+def generatePascal(rowMax):
+  for n in range(rowMax):
+    i = n
+    for k in range(i):
+      print(str(nCr(n,k)))
