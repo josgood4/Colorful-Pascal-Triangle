3db878e21858fa8f842f46fc4f16c17fe3f7cc93
josgood4 <jwosgood4@gmail.com>
2017-04-04 21:45:41 -0400

    Drawing the pyramid - take 1
    Still need to: make adjustable widths and heights (aka get rid of the
    magic number)

--- Pascals_0.py
+++ Pascals_0.py
@@ -1,6 +1,9 @@
 from turtle import *
 import math
 
+COLOR_LIST = ['red', 'blue', 'green', 'yellow', 'purple', \
+              'black', 'green4', 'blue4', 'DeepSkyBlue', 'violet']
+
 # calculates nCr
 def nCr(n,r):
   if n >= r and r >= 0:
@@ -20,10 +23,59 @@ def genModPascal(mod, rowMax):
     outerList.append(innerList)
   return outerList
 
+
 # print the result of generatePascal() - for testing purposes
 def testGeneratePascal(mod, rowMax):
   triangle = genModPascal(mod, rowMax)
+  t = Turtle()
+  t.ht()
   for eachRow in triangle:
-    print(eachRow)
+    ##print(eachRow)
+    drawRow(t, eachRow)
+    
+
+# draws the given row with the turtle
+# @param t - a turtle object
+# @param row - a list to draw 
+# note: position the turtle in the upper left corner of the desired row, facing east
+#       the turtle ends just below the lower left corner, facing the same way
+def drawRow(t, row):
+  l = len(row)
+  if l == 1:
+    print("l=0")
+    for stOffset in (1,0):
+      t.color(COLOR_LIST[row[0]])
+      t.pu()
+      t.fd(stOffset)
+      t.pd()
+      t.fd(4-stOffset*2)
+      t.pu()
+      t.fd(stOffset)
+      # reset 
+      t.bk(4)
+      t.rt(90)
+      t.fd(1)
+      t.lt(90)
+    
+  else:
+    for stOffset in (2,1,0):
+      for i in range(len(row)):
+        t.color(COLOR_LIST[row[i]])
+        if i==0:
+          t.pu()
+          t.fd(stOffset)
+          t.pd()
+          t.fd(5-stOffset)
+        elif i < len(row)-1:
+          t.fd(4)
+        else:
+          t.fd(5-stOffset)
+          t.pu()
+          t.fd(stOffset)
+      t.bk(10 + (l-2)*4)
+      t.rt(90)
+      t.fd(1)
+      t.lt(90)
+  t.bk(2)
 
 testGeneratePascal(2, 16)
